fb1057b7e2a7418d35d602c55946d43f2b140045
Aotokitsuruya <contact@aotoki.me>
2024-01-09 21:20:41 +0800

    Use environment variable to find function name than context

--- lib/hanami/lambda/dispatcher.rb
+++ lib/hanami/lambda/dispatcher.rb
@@ -39,7 +39,7 @@ module Hanami
       #
       # @return [Handler] the handler
       def lookup(event:, context:) # rubocop:disable Lint/UnusedMethodArgument
-        function_name = context.function_name
+        function_name = ENV.fetch("AWS_LAMBDA_FUNCTION_NAME", context.function_name)
         handlers
           .select { |name, _| function_name.include?(name) }
           .max_by { |name, _| name.length }
